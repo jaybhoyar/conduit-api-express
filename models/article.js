18058b8c3da9064d8903674aac4bdbac26c9948a
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-02-28 11:41:54 +0530

    slugify add

---
+++ models/article.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+var { hash, compare } = require("bcryptjs");
+
+var articleSchema = 
+module.exports = mongoose.model("Article", userSchema);
